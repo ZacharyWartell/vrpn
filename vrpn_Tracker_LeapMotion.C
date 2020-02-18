3a14f8ffb51b1a19107ab118e0670163a194ec7f
Zachary Wartell <zwartell@uncc.edu>
2020-02-18 00:22:28 -0500

    -[wip]

--- vrpn_Tracker_LeapMotion.C
+++ vrpn_Tracker_LeapMotion.C
@@ -29,11 +29,21 @@ Stage I - copy code from vrpn_Tracker_NULL
 #endif
 #endif
 
+#include "quat.h"
 #include "vrpn_RedundantTransmission.h" // for vrpn_RedundantTransmission
 #include "vrpn_Tracker_LeapMotion.h"
-#include "quat.h"
 
 /* END COPY FROM vrpn_Tracker.C */
+
+/**
+
+[Stage I] Temporary hack
+*/
+#if 0
+#define main HIDE_MAIN
+#include "submodules\LeapSDK\samples\Sample.cpp"
+#undef main
+#endif
 
 /*
 @uathor Zachary Wartell
@@ -43,14 +53,26 @@ Stage I - copy code from vrpn_Tracker_NULL
 */
 vrpn_Tracker_LeapMotion::vrpn_Tracker_LeapMotion(const char *name,
                                                  vrpn_Connection *c,
-                                     vrpn_int32 sensors, vrpn_float64 Hz)
+                                                 vrpn_int32 sensors,
+                                                 vrpn_float64 Hz)
     : vrpn_Tracker(name, c)
     , update_rate(Hz)
     , d_redundancy(NULL)
 {
     num_sensors = sensors;
     register_server_handlers();
-    // Nothing left to do
+
+    // Create a sample listener and controller
+
+    // Have the sample listener receive events from the controller
+    controller.addListener(listener);
+
+#if 0
+    // [FIX-ME] from Sample.cpp
+    if (argc > 1 && strcmp(argv[1], "--bg") == 0)
+        controller.setPolicy(Leap::Leap::Controller::POLICY_BACKGROUND_FRAMES);
+#endif
+
 }
 
 /*
@@ -153,7 +175,225 @@ void vrpn_Tracker_LeapMotion::mainloop()
 
 - Stage I - copy code from vrpn_Tracker_NULL
 */
-void vrpn_Tracker_LeapMotion::setRedundantTransmission(vrpn_RedundantTransmission *t)
+void vrpn_Tracker_LeapMotion::setRedundantTransmission(
+    vrpn_RedundantTransmission *t)
 {
     d_redundancy = t;
 }
+
+vrpn_Tracker_LeapMotion::~vrpn_Tracker_LeapMotion() 
+{
+    // Remove the sample listener when done
+    controller.removeListener(listener);
+}
+
+
+static const std::string fingerNames[] = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
+static const std::string boneNames[] = {"Metacarpal", "Proximal", "Middle",
+                                        "Distal"};
+static const std::string stateNames[] = {"STATE_INVALID", "STATE_START",
+                                  "STATE_UPDATE", "STATE_END"};
+
+
+/*
+BEGIN COPY FROM Leap SDK Sample.cpp
+*/
+void vrpn_Tracker_LeapMotion::Listener::onInit(const Leap::Controller& controller)
+{
+    std::cout << "Initialized" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onConnect(const Leap::Controller& controller)
+{
+    std::cout << "Connected" << std::endl;
+    controller.enableGesture(Leap::Gesture::TYPE_CIRCLE);
+    controller.enableGesture(Leap::Gesture::TYPE_KEY_TAP);
+    controller.enableGesture(Leap::Gesture::TYPE_SCREEN_TAP);
+    controller.enableGesture(Leap::Gesture::TYPE_SWIPE);
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onDisconnect(const Leap::Controller& controller)
+{
+    // Note: not dispatched when running in a debugger.
+    std::cout << "Disconnected" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onExit(const Leap::Controller& controller)
+{
+    std::cout << "Exited" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onFrame(const Leap::Controller& controller)
+{
+    // Get the most recent frame and report some basic information
+    const Leap::Frame frame = controller.frame();
+    std::cout << "Leap::Frame id: " << frame.id()
+              << ", timestamp: " << frame.timestamp()
+              << ", hands: " << frame.hands().count()
+              << ", extended fingers: " << frame.fingers().extended().count()
+              << ", tools: " << frame.tools().count()
+              << ", gestures: " << frame.gestures().count() << std::endl;
+
+    Leap::HandList hands = frame.hands();
+    for (Leap::HandList::const_iterator hl = hands.begin(); hl != hands.end();
+         ++hl) {
+        // Get the first hand
+        const Leap::Hand hand = *hl;
+        std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+        std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+                  << ", palm position: " << hand.palmPosition() << std::endl;
+        // Get the hand's normal vector and direction
+        const Leap::Vector normal = hand.palmNormal();
+        const Leap::Vector direction = hand.direction();
+
+        // Calculate the hand's pitch, roll, and yaw angles
+        std::cout << std::string(2, ' ')
+                  << "pitch: " << direction.pitch() * Leap::RAD_TO_DEG << " degrees, "
+                  << "roll: " << normal.roll() * Leap::RAD_TO_DEG
+                  << " degrees, "
+                  << "yaw: " << direction.yaw() * Leap::RAD_TO_DEG << " degrees"
+                  << std::endl;
+
+        // Get the Arm bone
+        Leap::Arm arm = hand.arm();
+        std::cout << std::string(2, ' ') << "Arm direction: " << arm.direction()
+                  << " wrist position: " << arm.wristPosition()
+                  << " elbow position: " << arm.elbowPosition() << std::endl;
+
+        // Get fingers
+        const Leap::FingerList fingers = hand.fingers();
+        for (Leap::FingerList::const_iterator fl = fingers.begin();
+             fl != fingers.end(); ++fl) {
+            const Leap::Finger finger = *fl;
+            std::cout << std::string(4, ' ') << fingerNames[finger.type()]
+                      << " finger, id: " << finger.id()
+                      << ", length: " << finger.length()
+                      << "mm, width: " << finger.width() << std::endl;
+
+            // Get finger bones
+            for (int b = 0; b < 4; ++b) {
+                Leap::Bone::Type boneType = static_cast<Leap::Bone::Type>(b);
+                Leap::Bone bone = finger.bone(boneType);
+                std::cout << std::string(6, ' ') << boneNames[boneType]
+                          << " bone, start: " << bone.prevJoint()
+                          << ", end: " << bone.nextJoint()
+                          << ", direction: " << bone.direction() << std::endl;
+            }
+        }
+    }
+
+    // Get tools
+    const Leap::ToolList tools = frame.tools();
+    for (Leap::ToolList::const_iterator tl = tools.begin(); tl != tools.end();
+         ++tl) {
+        const Leap::Tool tool = *tl;
+        std::cout << std::string(2, ' ') << "Tool, id: " << tool.id()
+                  << ", position: " << tool.tipPosition()
+                  << ", direction: " << tool.direction() << std::endl;
+    }
+
+    // Get gestures
+    const Leap::GestureList gestures = frame.gestures();
+    for (int g = 0; g < gestures.count(); ++g) {
+        Leap::Gesture gesture = gestures[g];
+
+        switch (gesture.type()) {
+        case Leap::Gesture::TYPE_CIRCLE: {
+            Leap::CircleGesture circle = gesture;
+            std::string clockwiseness;
+
+            if (circle.pointable().direction().angleTo(circle.normal()) <=
+                Leap::PI / 2) {
+                clockwiseness = "clockwise";
+            }
+            else {
+                clockwiseness = "counterclockwise";
+            }
+
+            // Calculate angle swept since last frame
+            float sweptAngle = 0;
+            if (circle.state() != Leap::Gesture::STATE_START) {
+                Leap::CircleGesture previousUpdate = Leap::CircleGesture(
+                    controller.frame(1).gesture(circle.id()));
+                sweptAngle = (circle.progress() - previousUpdate.progress()) *
+                             2 * Leap::PI;
+            }
+            std::cout << std::string(2, ' ') << "Circle id: " << gesture.id()
+                      << ", state: " << stateNames[gesture.state()]
+                      << ", progress: " << circle.progress()
+                      << ", radius: " << circle.radius() << ", angle "
+                      << sweptAngle * Leap::RAD_TO_DEG << ", " << clockwiseness
+                      << std::endl;
+            break;
+        }
+        case Leap::Gesture::TYPE_SWIPE: {
+            Leap::SwipeGesture swipe = gesture;
+            std::cout << std::string(2, ' ') << "Swipe id: " << gesture.id()
+                      << ", state: " << stateNames[gesture.state()]
+                      << ", direction: " << swipe.direction()
+                      << ", speed: " << swipe.speed() << std::endl;
+            break;
+        }
+        case Leap::Gesture::TYPE_KEY_TAP: {
+            Leap::KeyTapGesture tap = gesture;
+            std::cout << std::string(2, ' ') << "Key Tap id: " << gesture.id()
+                      << ", state: " << stateNames[gesture.state()]
+                      << ", position: " << tap.position()
+                      << ", direction: " << tap.direction() << std::endl;
+            break;
+        }
+        case Leap::Gesture::TYPE_SCREEN_TAP: {
+            Leap::ScreenTapGesture screentap = gesture;
+            std::cout << std::string(2, ' ')
+                      << "Screen Tap id: " << gesture.id()
+                      << ", state: " << stateNames[gesture.state()]
+                      << ", position: " << screentap.position()
+                      << ", direction: " << screentap.direction() << std::endl;
+            break;
+        }
+        default:
+            std::cout << std::string(2, ' ') << "Unknown gesture type."
+                      << std::endl;
+            break;
+        }
+    }
+
+    if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
+        std::cout << std::endl;
+    }
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onFocusGained(const Leap::Controller& controller)
+{
+    std::cout << "Focus Gained" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onFocusLost(const Leap::Controller& controller)
+{
+    std::cout << "Focus Lost" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onDeviceChange(const Leap::Controller& controller)
+{
+    std::cout << "Device Changed" << std::endl;
+    const Leap::DeviceList devices = controller.devices();
+
+    for (int i = 0; i < devices.count(); ++i) {
+        std::cout << "id: " << devices[i].toString() << std::endl;
+        std::cout << "  isStreaming: "
+                  << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+    }
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onServiceConnect(const Leap::Controller& controller)
+{
+    std::cout << "Service Connected" << std::endl;
+}
+
+void vrpn_Tracker_LeapMotion::Listener::onServiceDisconnect(const Leap::Controller& controller)
+{
+    std::cout << "Service Disconnected" << std::endl;
+}
+/*
+END COPY FROM Leap SDK Sample.cpp
+*/
--- vrpn_Tracker_LeapMotion.h
+++ vrpn_Tracker_LeapMotion.h
@@ -1,15 +1,22 @@
 #ifndef vrpn_LEAP_MOTION_TRACKER_H
 #define vrpn_LEAP_MOTION_TRACKER_H
+/*
+@author Zachary Wartell
+@brief
+
+Stage I - copy code from vrpn_Tracker_NULL
+
+*/
 #include <stdio.h> // for NULL, FILE
 
 /*
 @author Zachary Wartell
 
 */
+#include "Leap.h"
 #include "vrpn_Tracker.h"
 
 class VRPN_API vrpn_RedundantTransmission;
-
 
 // This is an example of a tracker server.  It basically reports the
 // position at the origin with zero velocity and acceleration over and
@@ -25,11 +32,10 @@ class VRPN_API vrpn_RedundantTransmission;
 class VRPN_API vrpn_Tracker_LeapMotion : public vrpn_Tracker {
 public:
     vrpn_Tracker_LeapMotion(const char *name, vrpn_Connection *c,
-                      vrpn_int32 sensors = 1, vrpn_float64 Hz = 1.0);
-    
+                            vrpn_int32 sensors = 1, vrpn_float64 Hz = 1.0);
+    ~vrpn_Tracker_LeapMotion();
     /// This function should be called each time through app mainloop.
     virtual void mainloop();
-
 
     void setRedundantTransmission(vrpn_RedundantTransmission *);
 
@@ -37,6 +43,25 @@ protected:
     vrpn_float64 update_rate;
 
     vrpn_RedundantTransmission *d_redundancy;
+
+private:
+    class Listener : public Leap::Listener {
+    public:
+        virtual void onInit(const Leap::Controller &);
+        virtual void onConnect(const Leap::Controller &);
+        virtual void onDisconnect(const Leap::Controller &);
+        virtual void onExit(const Leap::Controller &);
+        virtual void onFrame(const Leap::Controller &);
+        virtual void onFocusGained(const Leap::Controller &);
+        virtual void onFocusLost(const Leap::Controller &);
+        virtual void onDeviceChange(const Leap::Controller &);
+        virtual void onServiceConnect(const Leap::Controller &);
+        virtual void onServiceDisconnect(const Leap::Controller &);
+
+    private:
+    };
+    Listener listener;
+    Leap::Controller controller;
 };
 
 // End of vrpn_LEAP_MOTION_TRACKER_H
